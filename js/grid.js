c593a16dbc142a06d938d989c65aa6ee7eadb12e
Martin Aarhus Gregersen <marting12@live.dk>
2015-04-08 23:58:45 +0200

    My etch-a-sketch program, created following a tutorial

---
+++ js/grid.js
@@ -0,0 +1,344 @@
+$(document).ready(function() {
+// Create a div and insert it into the html
+	console.log("If you're reading this, you're more knowledgable about your browswer than roughly 70% of users, congratulations!");
+
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row1').appendTo("div#wrapper")
+
+	// Row 2
+
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row2').appendTo("div#wrapper")
+
+	// Row 3
+
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row3').appendTo("div#wrapper")
+
+	// Row 4
+
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row4').appendTo("div#wrapper")
+
+	// Row 5
+
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row5').appendTo("div#wrapper")
+
+	// Row 6
+
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row6').appendTo("div#wrapper")
+
+	// Row 7
+
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row7').appendTo("div#wrapper")
+
+	// Row 8
+
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row8').appendTo("div#wrapper")
+
+	// Row 9
+
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row9').appendTo("div#wrapper")
+
+	// Row 10
+
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row10').appendTo("div#wrapper")
+
+	// Row 11
+
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row11').appendTo("div#wrapper")
+
+	// Row 12
+
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row12').appendTo("div#wrapper")
+
+	// Row 13
+
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row13').appendTo("div#wrapper")
+
+	// Row 14
+
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row14').appendTo("div#wrapper")
+
+	// Row 15
+
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row15').appendTo("div#wrapper")
+
+	// Row 16
+
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+	$("<div></div>").attr('class', 'row16').appendTo("div#wrapper")
+
+// Create classes for different divs. 
+$(".row1").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row2").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row3").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row4").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row5").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row6").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row7").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row8").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row9").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row10").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row11").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row12").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row13").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row14").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row15").css({"display":"inline-block","height":"25px","width":"25px"});
+$(".row16").css({"display":"inline-block","height":"25px","width":"25px"});
+
+// Add a hover effect
+	$(".row1, .row2, .row3, .row4, .row5, .row6, .row7, .row8, .row9, .row10, .row11, .row12, .row13, .row14, .row15, .row16").mouseenter( function() {
+		$(this).toggleClass("active");
+	});
+// Add a clear button
+	$("button").click(function() {
+		$("*").removeClass("active")
+		// var amount = prompt("How many squares per side would you like? 16, 32 or 64?");
+		// if(amount === "16") {
+		// 	$("<p>16x16 Grid size</p>").appendTo("#gridsize");
+		// } else if (amount === "32") {
+		// 	$("<p>32x32 Grid size</p>").appendTo("#gridsize");
+		// } else if(amount === "64") {
+		// 	$("<p>64x64 Grid size</p>").appendTo("#gridsize");
+		// } else {
+		// 	alert("You didn't type '16', '32' or '64'. Please try again")
+		// }
+	});
+
+});
